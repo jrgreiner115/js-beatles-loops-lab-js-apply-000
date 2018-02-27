dbf5c113db9e4942d0f5f9b2b59f001181e85893
Joe Greiner <jrgreiner115@gmail.com>
2018-02-27 02:38:23 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,10 @@
+function theBeatlesPlay(musicians, instruments) {
+ var array = [];
+ for (var i = 0; i < musicians.length; i++) {array.push(musicians[i] + ' plays ' + instruments[i]+'.')}
+ return array
+}
+function johnLennonFacts(facts) {
+  var factsArray = []
+  var x = 0
+  while (x < facts.length) {factsArray.push(facts[x] + '!!!'), x++
+}}
